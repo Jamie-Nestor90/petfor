3aff0a13fe3cbd939b3b34ae04fd73e096a55139
Jamie Yoo <naive0612@gmail.com>
2024-04-16 17:15:58 +0900

    이클립스 깃 테스트

--- src/main/java/member/Member.java
+++ src/main/java/member/Member.java
@@ -2,7 +2,7 @@ package member;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
+// import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
 import javax.naming.Context;
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,7 +4,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Insert title here</title>
+<title>로그인 페이지</title>
 </head>
 <body>
    <h1> 회원가입 </h1>
